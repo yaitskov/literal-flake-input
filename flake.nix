61eb676567e629e0499d5847e91d4de37787a444
Daniil Iaitskov <dyaitskov@gmail.com>
2026-02-09 20:19:29 +0300

    master static bulid

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 {
   description = "VPN bypass";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/ec57e782751e30b7b97907a4b14a7b584e14f3ec";
+    nixpkgs.url = "github:NixOS/nixpkgs/bc16855ba53f3cb6851903a393e7073d1b5911e7";
     flake-utils.url = "github:numtide/flake-utils";
     uphack = {
       url = "github:yaitskov/upload-doc-to-hackage";
@@ -15,12 +15,68 @@
       let
         ghcName = "ghc9122";
         packageName = "literal-flake-input";
+        mkStatic = pkName:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              crossSystem = "x86_64-unknown-linux-musl";
+              overlays = [
+                (final: prev: {
+                  haskell = prev.haskell // {
+                    compiler = prev.haskell.compiler // {
+                      ${ghcName} = prev.haskell.compiler.${ghcName}.override {
+                        enableRelocatedStaticLibs = true;
+                        enableShared = false;
+                        enableDwarf = false;
+                      };
+                    };
+                  };
+                })
+              ];
+            };
+            staticExtraLibs = [
+              "--ghc-option=-optl=-static"
+              "--extra-lib-dirs=${pkgs.gmp6.override { withStatic = true; }}/lib"
+              "--extra-lib-dirs=${pkgs.numactl.overrideAttrs (old: { dontDisableStatic = true; })}/lib"
+              "--extra-lib-dirs=${pkgs.zlib.static}/lib"
+              "--extra-lib-dirs=${pkgs.libelf.overrideAttrs (old: { dontDisableStatic = true; })}/lib"
+              "--extra-lib-dirs=${pkgs.libffi.overrideAttrs (old: { dontDisableStatic = true; })}/lib"
+            ];
+
+            assertStatic = drv:
+              drv.overrideAttrs(oa: {
+                postInstall = (oa.postInstall or "") + ''
+                  for b in $out/bin/*
+                  do
+                    if ldd "$b"
+                    then
+                      echo "ldd succeeded on $b, which may mean that it is not statically linked"
+                      exit 1
+                    fi
+                  done
+                '';});
+
+            makeStatic = drv:
+              drv.overrideAttrs(oa:
+                { configureFlags = (oa.configureFlags or []) ++ staticExtraLibs; });
+
+            haskellPackagesO = pkgs.haskell.packages.${ghcName};
+            inherit (pkgs.haskell.lib) dontCheck justStaticExecutables;
+            haskellPackages = haskellPackagesO.override {
+              overrides = final: prev: {
+                vector = dontCheck prev.vector;
+              };
+            };
+          in
+            assertStatic (makeStatic (justStaticExecutables
+              (haskellPackages.callCabal2nix pkName self rec {})));
         pkgs = nixpkgs.legacyPackages.${system};
         haskellPackages = pkgs.haskell.packages.${ghcName};
         inherit (pkgs.haskell.lib) dontHaddock;
       in {
         packages.${packageName} = dontHaddock (haskellPackages.callCabal2nix packageName self rec {});
         packages.default = self.packages.${system}.${packageName};
+        packages.static = mkStatic packageName;
         defaultPackage = self.packages.${system}.default;
 
         devShells.default = pkgs.mkShell {
