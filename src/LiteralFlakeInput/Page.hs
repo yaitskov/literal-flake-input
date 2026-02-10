c682ec4464258c1b95d73cb3d9e71cf440edd35f
Daniil Iaitskov <dyaitskov@gmail.com>
2026-02-10 10:44:41 +0300

    dont create aes file

--- src/LiteralFlakeInput/Page.hs
+++ src/LiteralFlakeInput/Page.hs
@@ -5,9 +5,9 @@ module LiteralFlakeInput.Page where
 
 import LiteralFlakeInput.Nix ( NixDer, translate )
 import LiteralFlakeInput.Prelude
-    ( ($), Applicative(pure), Semigroup((<>)), show )
+    ( ($), Applicative(pure), Semigroup((<>)), show, Maybe(..) )
 import Yesod.Core
-    ( Yesod,
+    ( Yesod(..),
       RenderRoute(renderRoute),
       Texts,
       mkYesod,
@@ -21,7 +21,8 @@ mkYesod "Ypp" [parseRoutes|
 /*{Texts} HomeR GET
 |]
 
-instance Yesod Ypp
+instance Yesod Ypp where
+  makeSessionBackend _ = pure Nothing
 
 getHomeR :: Texts -> Handler NixDer
 getHomeR params = do
