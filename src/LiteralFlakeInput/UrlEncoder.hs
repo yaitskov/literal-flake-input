4ff6170ba918ef16619ea01c348bfa461755b647
Daniil Iaitskov <dyaitskov@gmail.com>
2026-02-13 19:48:42 +0300

    use wl-pprint in UrlEncoder for error reporting

--- src/LiteralFlakeInput/UrlEncoder.hs
+++ src/LiteralFlakeInput/UrlEncoder.hs
@@ -17,6 +17,7 @@ import Nix
 import Nix.Standard ( runWithBasicEffectsIO )
 import Paths_literal_flake_input ( version )
 import System.Environment.Blank (getEnvDefault)
+import Text.PrettyPrint.Leijen.Text (linebreak, text, putDoc, vcat, indent)
 import Text.Regex.TDFA ( (=~) )
 
 
@@ -25,17 +26,21 @@ runUrlEncoder = runUrlEncoderWith . fmap toText =<< getArgs
 
 runUrlEncoderWith :: [Text] -> IO ()
 runUrlEncoderWith args
-  | args `elem` [ [], ["--version"], ["-v"]] =
+  | args `elem` [["--version"], ["-v"]] = do
     putStrLn $ "Literal Flake Input " <> showVersion version
-  | args `elem` [[], ["-h"], ["-help"], ["--help"], ["-help"], ["help"]] =
+    exitFailure
+  | args `elem` [[], ["-h"], ["-help"], ["--help"], ["-help"], ["help"]] = do
     putStrLn """Literal Flake Input (LFI) URL encoder
-      Usage: nix build $(e -attr-name true \\
-                         -attr-name2 null \\
-                         -an3 12 \\
-                         -an4 hello world \\
-                         -an5 [1 2] \\
-                         -an7 "{ x = 1; y = 2; }" \\
-                         -an7 x: x + 1)
+      Usage: nix build $(e -an1 true \\
+                           -an2 null \\
+                           -an3 12 \\
+                           -an4 hello world \\
+                           -an5 [ 1 2 ] \\
+                           -an6 "{x = 1; y = 2; }" \\
+                           -an7 x: x + 1)
+
+      The result is a non-flake URL input:
+      nix build --override-input c https://lficom.me/an1/true/an2/null/an3/42/an4/%22hello%20world%22/an5/%5B%201%202%20%5D/an6/%7Bx%20=%201%3B%20y%20=%202%3B%20%7D/an7/a:%20a%20+%201/.tar
 
       LFI translates arbitrary command line arguments into a Nix attribute set.
       Nix values are verified with hnix interpreter.
@@ -43,6 +48,7 @@ runUrlEncoderWith args
       URL prefix can be overriden via environment variable LFI_SITE.
 
       Project home page https://github.com/yaitskov/literal-flake-input"""
+    exitFailure
   | length args == 1 = do
     putStrLn """LFI expects "even" number of argument. See help by -h or --help"""
     exitFailure
@@ -53,18 +59,23 @@ runUrlEncoderWith args
         time <- getCurrentTime
         let opts = defaultOptions time
         (goodAtrs, badAtrs) <- M.partition isRight <$> M.traverseWithKey (go opts) m
-        if null badAtrs
-          then do
-          siteRoot <- toText <$> getEnvDefault "LFI_SITE" siteRootDef
-          putLBSLn . mkLfiUrl siteRoot . M.mapKeys argToAtr $ M.mapMaybe rightToMaybe goodAtrs
-          else do
-          putStrLn $ "Invalid attribute(s):\n" <> show (M.mapMaybe leftToMaybe  badAtrs)
-          exitFailure
+        if null badAtrs then
+          do
+            siteRoot <- toText <$> getEnvDefault "LFI_SITE" siteRootDef
+            putLBSLn . mkLfiUrl siteRoot . M.mapKeys argToAtr $ M.mapMaybe rightToMaybe goodAtrs
+          else
+          do
+            putDoc $ text "Invalid attribute(s):" <> linebreak <>
+              idnt4 (vcat . mapMaybe leftToMaybe $ M.elems badAtrs) <>
+              linebreak
+            exitFailure
   where
+    idnt4 = indent 4
     siteRootDef = "https://lficom.me"
     go opts (ArgName an) av = do
       validateNixExpr opts av >>= \case
-        Left e -> pure . Left $ "Attribute " <> an <> " " <> e
+        Left e -> pure . Left $
+          (text (toLazy an) <> linebreak <> (idnt4 . vcat . fmap (text . toLazy) $ lines e))
         Right () -> pure $ Right av
 
 newtype ArgName = ArgName Text deriving newtype (Eq, Show, Ord)
@@ -81,7 +92,7 @@ groupByAttrName [] = Right M.empty
 groupByAttrName [a] = Left $ "Attribute [" <> a <> "] has no value"
 groupByAttrName (atrN:argsLeft)
   | isAtrName atrN =
-      case span isAtrName argsLeft of
+      case break isAtrName argsLeft of
         (atrVs, nextAtr) ->
           case nonEmpty atrVs of
             Nothing -> Left $ "Attribute [" <> atrN <> "] has no value"
@@ -123,6 +134,7 @@ mkLfiUrl siteRoot w =
     "--override-input c " <>
     fromByteString (encodeUtf8 siteRoot) <>
     encodePathSegments (flatpairs $ M.toList w)
+    <> "/.tar"
   where
    flatpairs :: [(AtrName, Text)] -> [Text]
    flatpairs [] = []
