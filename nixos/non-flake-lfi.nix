8f382259bbbe6459b8260cfed2ccd492fd80ef42
Daniil Iaitskov <dyaitskov@gmail.com>
2026-02-13 23:37:59 +0300

    fix non-flake-lfi

--- nixos/non-flake-lfi.nix
+++ nixos/non-flake-lfi.nix
@@ -3,6 +3,6 @@
 , pkgs
 , ...
 }: let
-  #literal-flake-input-router = (builtins.getFlake "github:yaitskov/literal-flake-input").packages.x86_64-linux.vpn-router;
-  lfi = (builtins.getFlake "path:/home/don/pro/haskell/my/literal-flake-input/literal-flake-input").packages.x86_64-linux.vpn-router;
+  #lfi = (builtins.getFlake "github:yaitskov/literal-flake-input").packages.x86_64-linux.literal-flake-input;
+  lfi = (builtins.getFlake "path:/home/don/pro/haskell/my/literal-flake-input/literal-flake-input").packages.x86_64-linux.literal-flake-input;
 in (import ./flake-lfi.nix lfi) { inherit config; inherit lib; inherit pkgs; }
